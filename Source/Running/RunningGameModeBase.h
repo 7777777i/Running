59209a9fd03c066a1f55332b3c9986c7cbcc21b2
7777777i <3185693642@qq.com>
2023-06-03 15:02:55 +0800

    init running

---
+++ Source/Running/RunningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RunningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNNING_API ARunningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
